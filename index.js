1a66c14c32140c21fe160264ffe707d629952bd1
Mandell Butler <mandell.butler@outlook.com>
2021-05-05 23:03:16 -0400

    Added Readme via nodejs

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ const renderReadme = ({
     tests,
     username,
     email,
-}) => {
+  }) => {
     const templateReadMe = `
     ## ${title}
     
@@ -137,64 +137,3 @@ const renderReadme = ({
 
 // init();
 renderQuestions(questions);
-
-//prompt user for information to generate readme file
-
-//===============
-// inquirer
-//   .prompt(questions)
-//   .then(answers => {
-//       fs.writeFile("answers.txt", JSON.stringify(answers), "utf8", (err) => {
-//           if (err) console.log(err);
-//             console.log("Success!")
-//       })
-//     // Use user feedback for... whatever!!
-//   })
-//   .catch(error => {
-//     if(error.isTtyError) {
-//       // Prompt couldn't be rendered in the current environment
-//     } else {
-//       // Something else went wrong
-//     }
-// });
-
-
-// GIVEN a command-line application that accepts user input
-// WHEN I am prompted for information about my application repository
-// THEN a high-quality, professional README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
-
-// WHEN I enter my project title
-// THEN this is displayed as the title of the README
-        //PROMPT for INPUT: Title
-        //PROMPT for INPUT: Description
-        //PROMPT for INPUT: Table of Contents
-                // WHEN I click on the links in the Table of Contents
-                // THEN I am taken to the corresponding section of the README
-        
-
-// WHEN I choose a license for my application from a list of options
-// THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
-        //PROMPT for CHOICES: License
-
-// WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
-// THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
-
-        //PROMPT for INPUT: Installation
-        //PROMPT for INPUT: Usage
-        //PROMPT for INPUT: Contributing
-        //PROMPT for INPUT: Tests
-
-
-// WHEN I enter my GitHub username
-// THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
-// WHEN I enter my email address
-// THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
-        //PROMPT for INPUT: Questions
-
-
-
-
-
-
-
-
